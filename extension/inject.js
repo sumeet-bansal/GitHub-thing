c74609bb8cc362fe618ac311c29ee62f4d32e537
Sumeet Bansal <sumeetbansal@gmail.com>
2017-09-26 10:19:40 -0700

    added basic text inject

--- extension/inject.js
+++ extension/inject.js
@@ -2,3 +2,18 @@
  * 1. get latest commit
  * 2. inject latest commit
  */
+
+var repos = document.getElementById('user-repositories-list').childNodes[1];
+repos = repos.getElementsByTagName('li');
+
+for (var i = 0; i < repos.length; i++) {
+	var repo = repos[i];
+	var el = document.createElement('div');
+	el.className = 'f6 text-gray mt-2';
+	var elspan = document.createElement('span');
+	elspan.setAttribute('class', 'mr-3');
+	elspan.textContent = "test";
+	el.appendChild(elspan)
+	repo.appendChild(el);
+	console.log(repo);
+}
